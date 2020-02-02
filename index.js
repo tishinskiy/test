47b0914a293fa870a39bc9a9825c7109c21be409
Alexandr Tishinskiy <tishinskiy@mail.ru>
2020-02-02 18:29:22 +0700

    add assets

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+	// res.send('Hello World!');
+	res.sendFile(path.join(__dirname+'/public/index.html'));
 });
+
+app.use('/assets', express.static(__dirname + '/public/assets/'));
 
 app.listen(3000, function () {
 	console.log('Example app listening on port 3000!');
